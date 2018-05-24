f557dbd8cd937cebd270ecae509acbb6461fee33
Artur Mustafin <artur.mustafin@gmail.com>
2018-05-24 18:50:39 +0300

    added opencover.xml

--- opencover.cmd
+++ opencover.cmd
@@ -1,5 +1,6 @@
 set nuget=%userprofile%\.nuget\packages
 set OPENCOVER=%nuget%\opencover\4.6.519\tools
+dotnet build
 %OPENCOVER%\OpenCover.Console.exe -oldstyle -register:user -target:"C:\Program Files\dotnet\dotnet.exe" -targetargs:"test --no-build Build.Tests" -filter:"+[*]* -[*.Tests*]*" -output:opencover.xml
 
 
