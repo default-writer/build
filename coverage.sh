176c32a0fe5d4c17d29dfcf47538603cc27dda76
Artur Mustafin <artur.mustafin@gmail.com>
2018-05-25 17:47:02 +0300

    added scripts

---
+++ coverage.sh
@@ -0,0 +1,44 @@
+﻿#!/bin/bash
+
+set -e
+
+# Install OpenCover and ReportGenerator, and save the path to their executables.
+nuget install -Verbosity quiet -OutputDirectory packages -Version 4.6.519 OpenCover
+
+OPENCOVER=$PWD/packages/OpenCover.4.6.519/tools/OpenCover.Console.exe
+
+CONFIG=Release
+# Arguments to use for the build
+DOTNET_BUILD_ARGS="-c $CONFIG"
+# Arguments to use for the test
+DOTNET_TEST_ARGS="$DOTNET_BUILD_ARGS"
+
+echo CLI args: $DOTNET_BUILD_ARGS
+
+echo Restoring
+
+dotnet restore
+
+echo Building
+
+dotnet build $DOTNET_BUILD_ARGS
+
+echo Testing
+
+coverage=./coverage
+rm -rf $coverage
+mkdir $coverage
+
+dotnet test -f netcoreapp2.0 $DOTNET_TEST_ARGS Build.Tests/Build.Tests.csproj
+
+echo "Calculating coverage with OpenCover"
+$OPENCOVER \
+  -target:"c:\Program Files\dotnet\dotnet.exe" \
+  -targetargs:"test -f netcoreapp2.0 $DOTNET_TEST_ARGS Build.Tests/Build.Tests.csproj" \
+  -mergeoutput \
+  -hideskipped:File \
+  -output:$coverage/coverage.xml \
+  -oldStyle \
+  -filter:"+[Build*]* -[Build.*Tests*]*" \
+  -searchdirs:$testdir/bin/$CONFIG/netcoreapp2.0 \
+  -register:user
--- opencover.cmd
+++ opencover.cmd
@@ -1,5 +1,22 @@
-rem SET PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin;C:\msbuild\sonar-scanner-msbuild-4.2.0.1214-net46;C:\Users\artur\AppData\Local\Apps\OpenCover;%PATH%
 rem SET VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise
-rem "C:\msbuild\sonar-scanner-msbuild-4.2.0.1214-net46"\SonarScanner.MSBuild.exe begin /k:"build-core" /n:"build" /v:"1.0" /d:sonar.cs.opencover.reportsPaths="opencover.xml" /d:sonar.coverage.exclusions="**/*Test*.cs" /d:sonar.organization="hack2root-github" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="7eaa0a53a471f0280146430327e6a24d98a850af"
-rem OpenCover.Console.exe -oldstyle -output:"opencover.xml" -register:user -target:"%VSINSTALLDIR%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" -targetargs:"Build.Tests\bin\Debug\netcoreapp2.1\Build.Tests.dll"
-rem "C:\msbuild\sonar-scanner-msbuild-4.2.0.1214-net46"\SonarScanner.MSBuild.exe end /d:sonar.login="7eaa0a53a471f0280146430327e6a24d98a850af"
+rem SET PATH=C:\msbuild\sonar-scanner-msbuild-4.2.0.1214-net46;C:\Users\artur\AppData\Local\Apps\OpenCover;%VSINSTALLDIR%\MSBuild\15.0\Bin\amd64;%PATH%
+rem SonarScanner.MSBuild.exe begin^
+rem  /k:"build-core"^
+rem  /n:"build"^
+rem  /v:"1.0"^
+rem  /d:sonar.cs.opencover.reportsPaths="opencover.xml"^
+rem  /d:sonar.coverage.exclusions="**/*Test*.cs"^
+rem  /d:sonar.organization="hack2root-github"^
+rem  /d:sonar.host.url="https://sonarcloud.io"^
+rem  /d:sonar.login="7eaa0a53a471f0280146430327e6a24d98a850af"
+rem MSBuild.exe 
+rem dotnet build --configuration Release
+OpenCover.Console.exe^
+  -oldstyle^
+  -output:"opencover.xml"^
+  -register:user^
+  -target:"vstest.console.exe"^
+  -targetargs:"Build.Tests\bin\Release\netcoreapp2.1\Build.Tests.dll"
+rem %VSINSTALLDIR%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe
+rem SonarScanner.MSBuild.exe end^
+rem  /d:sonar.login="7eaa0a53a471f0280146430327e6a24d98a850af"
