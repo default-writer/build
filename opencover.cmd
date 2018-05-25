219b018da78f55ba35b8e6b75013cc8db9e84815
Artur Mustafin <artur.mustafin@gmail.com>
2018-05-25 22:22:43 +0300

    added scripts

--- opencover.cmd
+++ opencover.cmd
@@ -1,5 +1,3 @@
-rem SET VSINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise
-rem SET PATH=C:\msbuild\sonar-scanner-msbuild-4.2.0.1214-net46;C:\Users\artur\AppData\Local\Apps\OpenCover;%VSINSTALLDIR%\MSBuild\15.0\Bin\amd64;%PATH%
 SonarScanner.MSBuild.exe begin^
  /k:"build-core"^
  /n:"build"^
@@ -17,6 +15,5 @@ OpenCover.Console.exe^
  -register:user^
  -target:"vstest.console.exe"^
  -targetargs:"Build.Tests\bin\Release\netcoreapp2.1\Build.Tests.dll"
-%VSINSTALLDIR%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe
 SonarScanner.MSBuild.exe end^
  /d:sonar.login="7eaa0a53a471f0280146430327e6a24d98a850af"
