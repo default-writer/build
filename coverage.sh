c4adc19667d96ca928e840f16a505459e6a8246b
Artur Mustafin <artur.mustafin@gmail.com>
2018-05-27 11:03:59 +0300

    #28

--- coverage.sh
+++ coverage.sh
@@ -32,11 +32,11 @@ mkdir $coverage
 if [ -n "$SONARCLOUDTOKEN" ]
 then
 $SONARCLOUD begin \
-	/k:"build-core" \
-	/d:"sonar.host.url=https://sonarcloud.io" \
-	/d:"sonar.coverage.exclusions=Build.Tests/**" \
+	//key:"build-core" \
+	//d:"sonar.host.url=https://sonarcloud.io" \
+	//d:"sonar.coverage.exclusions=Build.Tests/**" \
 	//d:"sonar.login=$SONARCLOUDTOKEN" \
-	/d:"sonar.verbose=true"
+	//d:"sonar.verbose=true"
 fi
 
 echo Building
