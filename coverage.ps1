fd1cbcb65008525d0245197c101c16557f750dca
Artur Mustafin <artur.mustafin@gmail.com>
2018-05-27 15:42:34 +0300

    #33

--- coverage.ps1
+++ coverage.ps1
@@ -1,16 +1,19 @@
 $DOTNET = "dotnet"
 
-$OPENCOVER=$env:OPENCOVER
-$SONARCLOUD=$env:SONARCLOUD
+$OPENCOVER=./packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0/SonarCloud.MSBuild.dll
+$SONARCLOUD=./packages/OpenCover.4.6.519/tools/OpenCover.Console.exe
 $SONARCLOUDTOKEN=$env:SONARCLOUDTOKEN
+$CONFIG = Release
 
 & $DOTNET $SONARCLOUD begin `
- /k:build-core `
- /d:sonar.host.url=https://sonarcloud.io `
- /d:sonar.coverage.exclusions=Build.Tests/** `
- /d:sonar.login=$SONARCLOUDTOKEN `
- /d:sonar.verbose=true `
- /d:sonar.cs.opencover.reportsPaths=coverage/coverage.xml
+ /k:"build-core" `
+ /d:sonar.host.url="https://sonarcloud.io" `
+ /d:sonar.coverage.exclusions="Build.Tests/**" `
+ /d:sonar.login="$SONARCLOUDTOKEN" `
+ /d:sonar.verbose="true" `
+ /d:sonar.cs.opencover.reportsPaths="coverage/coverage.xml""
+
+ $DOTNET build --configuration $CONFIG
 
 & $OPENCOVER `
  -target:"c:\Program Files\dotnet\dotnet.exe" `
@@ -20,7 +23,7 @@ $SONARCLOUDTOKEN=$env:SONARCLOUDTOKEN
  -output:coverage/coverage.xml `
  -oldStyle `
  -filter:"+[Build*]* -[Build.Tests*]*" `
- -searchdirs:$testdir/bin/$CONFIG/netcoreapp2.1 `
+ -searchdirs:Build.Tests/bin/$CONFIG/netcoreapp2.1 `
  -register:user
 
-& $DOTNET $SONARCLOUD end /d:sonar.login=$env:SONARCLOUDTOKEN"
+& $DOTNET $SONARCLOUD end /d:sonar.login=$SONARCLOUDTOKEN"
--- install.sh
+++ install.sh
@@ -8,10 +8,7 @@ curl -o scanner.zip -L https://github.com/SonarSource/sonar-scanner-msbuild/rele
 unzip scanner.zip -d $(PWD)/packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0
 chmod +x $(PWD)/packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0/sonar-scanner-3.1.0.1141/bin/sonar-scanner
 
-export OPENCOVER=./packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0/SonarCloud.MSBuild.dll
-export SONARCLOUD=./packages/OpenCover.4.6.519/tools/OpenCover.Console.exe
-
-sonarcube=$(PWD)/.sonarcube
+sonarcube=/.sonarcube
 rm -rf $sonarcube
 mkdir $sonarcube
 
