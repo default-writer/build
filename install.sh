0ccb9b4f135a31f745bfe2de9306607f644f9b25
Artur Mustafin <artur.mustafin@gmail.com>
2018-05-27 15:32:09 +0300

    #32

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ set -e
 nuget install -OutputDirectory packages -Version 4.6.519 OpenCover
 nuget install -OutputDirectory packages -Version 0.7.0 coveralls.net
 curl -o scanner.zip -L https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/4.2.0.1214/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0.zip
-unzip scanner.zip $(PWD)/packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0
+unzip scanner.zip -d $(PWD)/packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0
 chmod +x $(PWD)/packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0/sonar-scanner-3.1.0.1141/bin/sonar-scanner
 
-export OPENCOVER=$(PWD)/packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0/SonarCloud.MSBuild.dll
-export SONARCLOUD=$(PWD)/packages/OpenCover.4.6.519/tools/OpenCover.Console.exe
+export OPENCOVER=./packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0/SonarCloud.MSBuild.dll
+export SONARCLOUD=./packages/OpenCover.4.6.519/tools/OpenCover.Console.exe
 
 sonarcube=$(PWD)/.sonarcube
 rm -rf $sonarcube
