c9e2687c34ae166b3c1d3f1328a777720a09a362
Artur Mustafin <artur.mustafin@gmail.com>
2018-05-28 14:26:29 +0300

    appveyour coverall integration

--- coveralls.sh
+++ coveralls.sh
@@ -1,6 +1,6 @@
 ﻿#!/bin/bash
 
-if [ -n "$COVERALLSREPOTOKEN" ]
+if [ -n "COVERALLSREPOTOKEN" ]
 then
   packages/coveralls.net.0.7.0/tools/csmacnz.Coveralls.exe --opencover -i Build.Tests/coverage.xml --useRelativePaths
 fi
