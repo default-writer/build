dafceba22558c21fe0ddb06648ff3fece79d4b71
Artur Mustafin <artur.mustafin@gmail.com>
2018-05-28 13:27:19 +0300

    appveyour cache

--- install.sh
+++ install.sh
@@ -2,11 +2,15 @@
 
 set -e
 
+if [ ! -d "$(PWD)/packages/coveralls.net.0.7.0" ]
 nuget install -OutputDirectory packages -Version 0.7.0 coveralls.net
+fi
 
+if [ ! -d "$(PWD)/packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0" ]
 curl -o scanner.zip -L https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/4.2.0.1214/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0.zip
 unzip scanner.zip -d $(PWD)/packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0
 chmod +x $(PWD)/packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0/sonar-scanner-3.1.0.1141/bin/sonar-scanner
+fi
 
 sonarcube=/.sonarcube
 rm -rf $sonarcube
