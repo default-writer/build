8019810948c522e89e08ab7cd1436aba31e399c6
Artur Mustafin <artur.mustafin@gmail.com>
2018-05-28 12:07:25 +0300

    #48

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -e
 
 nuget install -OutputDirectory packages -Version 4.6.519 OpenCover
 nuget install -OutputDirectory packages -Version 0.7.0 coveralls.net
+
 curl -o scanner.zip -L https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/4.2.0.1214/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0.zip
 unzip scanner.zip -d $(PWD)/packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0
 chmod +x $(PWD)/packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0/sonar-scanner-3.1.0.1141/bin/sonar-scanner
@@ -17,3 +18,7 @@ rm -rf $coverage
 mkdir $coverage
 
 dotnet restore
+
+dotnet packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0/SonarScanner.MSBuild.dll begin /d:sonar.login="$SONARCLOUDTOKEN" /k:"build-core" /d:sonar.host.url="https://sonarcloud.io" /n:"build" /v:"1.0" /d:sonar.cs.opencover.reportsPaths="Build.Tests/coverage.xml" /d:sonar.coverage.exclusions="**/*Test*.cs" /d:sonar.organization="hack2root-github" /d:sonar.verbose=true 
+dotnet test --no-build Build.Tests /p:CollectCoverage=true /p:CoverletOutputFormat=opencover --configuration Release 
+dotnet packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0/SonarScanner.MSBuild.dll end /d:sonar.login="$SONARCLOUDTOKEN" 
