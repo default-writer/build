e8a6f2e607408f4b1bae7113207ede853676b8ab
Artur Mustafin <artur.mustafin@gmail.com>
2018-05-28 12:11:23 +0300

    #49

--- install.sh
+++ install.sh
@@ -18,7 +18,3 @@ rm -rf $coverage
 mkdir $coverage
 
 dotnet restore
-
-dotnet packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0/SonarScanner.MSBuild.dll begin /d:sonar.login="$SONARCLOUDTOKEN" /k:"build-core" /d:sonar.host.url="https://sonarcloud.io" /n:"build" /v:"1.0" /d:sonar.cs.opencover.reportsPaths="Build.Tests/coverage.xml" /d:sonar.coverage.exclusions="**/*Test*.cs" /d:sonar.organization="hack2root-github" /d:sonar.verbose=true 
-dotnet test --no-build Build.Tests /p:CollectCoverage=true /p:CoverletOutputFormat=opencover --configuration Release 
-dotnet packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0/SonarScanner.MSBuild.dll end /d:sonar.login="$SONARCLOUDTOKEN" 
