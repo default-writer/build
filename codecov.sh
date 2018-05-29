7e8f045a56ed3791fdd8eceec1f3e25f9bb4002d
Artur Mustafin <artur.mustafin@gmail.com>
2018-05-30 01:30:21 +0300

    codecov

--- codecov.sh
+++ codecov.sh
@@ -6,4 +6,7 @@ set -e
 #chmod +x codecov
 #./codecov -f "Build.Tests/coverage.xml" -t $CODECOVTOKEN
 
-./packages/Codecov.1.0.3/tools/codecov.exe "Build.Tests/coverage.xml" -t $CODECOVTOKEN
+#./packages/Codecov.1.0.3/tools/codecov.exe "Build.Tests/coverage.xml" -t $CODECOVTOKEN
+
+OpenCover.Console.exe -register:user -target:"%xunit20%\xunit.console.x86.exe" -targetargs:".\Build.Tests\bin\Release\Build.Tests.dll -noshadow" -filter:"+[Build*]* -[*Test*,*Exception*]*" -output:".\Build.Tests\coverage.xml"
+codecov -f ".\Build.Tests\coverage.xml" $CODECOVTOKEN
