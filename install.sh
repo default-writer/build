395cbd6d53a9569c657dba5665372f8fd11eb10f
Artur Mustafin <artur.mustafin@gmail.com>
2018-05-30 02:04:30 +0300

    removed codecov

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ if [ ! -d $(PWD)/packages ]
 then
 	nuget install -OutputDirectory packages -Version 4.6.519 OpenCover
 	nuget install -OutputDirectory packages -Version 0.7.0 coveralls.net
-	nuget install -OutputDirectory packages -Version 1.0.3 codecov
 fi
 
 if [ ! -d $(PWD)/packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0 ]
@@ -15,9 +14,6 @@ then
 	unzip scanner.zip -d $(PWD)/packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0
 	chmod +x $(PWD)/packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0/sonar-scanner-3.1.0.1141/bin/sonar-scanner
 fi
-
-curl -s https://codecov.io/bash > codecov
-chmod +x codecov
 
 sonarcube=/.sonarcube
 rm -rf $sonarcube
