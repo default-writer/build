348182ea9afc593643b7d02c9354f753d73291ab
Artur Mustafin <artur.mustafin@gmail.com>
2018-05-30 03:43:30 +0300

    removed unused code

--- install.sh
+++ install.sh
@@ -14,11 +14,3 @@ then
 	unzip scanner.zip -d $(PWD)/packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0
 	chmod +x $(PWD)/packages/sonar-scanner-msbuild-4.2.0.1214-netcoreapp2.0/sonar-scanner-3.1.0.1141/bin/sonar-scanner
 fi
-
-sonarcube=/.sonarcube
-rm -rf $sonarcube
-mkdir $sonarcube
-
-coverage=./coverage
-rm -rf $coverage
-mkdir $coverage
