3c51e896d03ee2bf5842cb1976bd7eb7e712ccbc
Artur Mustafin <artur.mustafin@gmail.com>
2018-05-31 15:29:43 +0300

    test coverlet output

--- coveralls.sh
+++ coveralls.sh
@@ -4,5 +4,5 @@ set -e
 
 if [ -n "$COVERALLS_REPO_TOKEN" ]
 then
-  packages/coveralls.net.0.7.0/tools/csmacnz.Coveralls.exe --opencover -i Build.Tests/coverage.xml --useRelativePaths
+  packages/coveralls.net.0.7.0/tools/csmacnz.Coveralls.exe --opencover -i ./Build.Tests/coverage.xml --useRelativePaths
 fi
