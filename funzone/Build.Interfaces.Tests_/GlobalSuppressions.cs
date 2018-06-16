00c7e2f94de7c452c83a6e399a2857048ce8652a
hack2root <me@experimentalcommunity.org>
2018-06-16 12:21:26 +0300

    removed old files

--- funzone/Build.Interfaces.Tests_/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:Build.Interfaces.Tests.UnitTest1.Test1")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:Build.Interfaces.Tests.UnitTest1.Test2")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Major Bug", "S3453:Classes should not have only \"private\" constructors", Justification = "<Pending>", Scope = "type", Target = "~T:Build.Interfaces.Tests.Type1")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S1144:Unused private types or members should be removed", Justification = "<Pending>", Scope = "member", Target = "~M:Build.Interfaces.Tests.Type1.#ctor(Build.Interfaces.Tests.Arg1,Build.Interfaces.Tests.Arg2)")]
