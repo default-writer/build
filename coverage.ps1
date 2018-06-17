bf4e812ccc9ccf7e4bfbd6b80a721e4eeea9ce40
hack2root <me@experimentalcommunity.org>
2018-06-18 01:17:22 +0300

    set tot debug configuration

--- coverage.ps1
+++ coverage.ps1
@@ -3,7 +3,7 @@
 & dotnet add Build.Tests package --package-directory packages OpenCover
 & dotnet add Build.Tests package --package-directory packages coverlet.msbuild
 & packages/dotnet-sonarscanner begin /d:sonar.login="$env:SONARCLOUDTOKEN" /k:"build-core" /d:sonar.host.url="https://sonarcloud.io" /n:"build" /v:"1.0" /d:sonar.cs.opencover.reportsPaths="Build.Tests/coverage.xml" /d:sonar.coverage.exclusions="**/MyFun*.cs,**/*Test*.cs,**/*Exception*.cs,**/*Attribute*.cs" /d:sonar.organization="hack2root-github" /d:sonar.sourceEncoding="UTF-8"
-& dotnet build --configuration Release
-& dotnet test --configuration Release --no-build Build.Tests /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutputDirectory=test-results
+& dotnet build --configuration Debug
+& dotnet test --configuration Debug --no-build Build.Tests /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutputDirectory=test-results
 & packages/dotnet-sonarscanner end /d:sonar.login="$env:SONARCLOUDTOKEN"
-& packages/csmacnz.Coveralls --opencover -i Build.Tests/coverage.xml
+& packages/csmacnz.Coveralls --opencover -i Build.Tests/coverage.opencover.xml
