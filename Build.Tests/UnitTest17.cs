2ab52825e04c6254ada01ef3f8f8c26e565dbcd9
hack2root <me@experimentalcommunity.org>
2018-06-17 13:31:47 +0300

    added more tests to code coverage

--- Build.Tests/UnitTest17.cs
+++ Build.Tests/UnitTest17.cs
@@ -8,15 +8,25 @@ namespace Build.Tests.TestSet17
     public static class UnitTest
     {
         [Fact]
-        public static void TestSet7_Method7()
+        public static void TestSet17_Method1()
         {
             //TestSet7
             var container = new Container();
             container.RegisterType<Type>();
-
             var runtimeType = new RuntimeType(new InjectionAttribute("Build.Tests.TestSet17.Type"), typeof(Type), true);
             runtimeType.SetRuntimeInstance(RuntimeInstance.CreateInstance);
+            Assert.Throws<TypeInstantiationException>(() => runtimeType.CreateInstance("Build.Tests.TestSet17.Type"));
+        }
 
+        [Fact]
+        public static void TestSet17_Method2()
+        {
+            //TestSet7
+            var container = new Container();
+            container.RegisterType<Type>();
+            var runtimeType = new RuntimeType(new InjectionAttribute("Build.Tests.TestSet17.Type"), typeof(Type), true);
+            runtimeType.SetRuntimeInstance(RuntimeInstance.CreateInstance);
+            runtimeType.AddParameter(new RuntimeType(new InjectionAttribute(typeof(Type)), typeof(Type), true));
             Assert.Throws<TypeInstantiationException>(() => runtimeType.CreateInstance("Build.Tests.TestSet17.Type"));
         }
     }
