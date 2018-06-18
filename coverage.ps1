56c8eb7bc180b8a2091d751e3df7a4755dc805f1
hack2root <me@experimentalcommunity.org>
2018-06-18 07:09:56 +0300

    escaped command line parameter

--- coverage.ps1
+++ coverage.ps1
@@ -5,6 +5,6 @@
 & dotnet restore
 & packages/dotnet-sonarscanner begin /d:sonar.login="$env:SONARCLOUDTOKEN" /k:"build-core" /d:sonar.host.url="https://sonarcloud.io" /n:"build" /v:"1.0" /d:sonar.cs.opencover.reportsPaths="./coverage.opencover.xml" /d:sonar.coverage.exclusions="**/MyFun*.cs,**/*Test*.cs,**/*Exception*.cs,**/*Attribute*.cs" /d:sonar.organization="hack2root-github" /d:sonar.sourceEncoding="UTF-8"
 & dotnet build --configuration Release
-& dotnet test --configuration Release --no-build Build.Tests /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutputDirectory=../ /p:ExcludeByFile=\"**/MyFun*.cs,**/*Test*.cs,**/*Exception*.cs,**/*Attribute*.cs\"
+& dotnet test --configuration Release --no-build Build.Tests /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutputDirectory=../ /p:ExcludeByFile=\"**\/MyFun*.cs,**\/*Test*.cs,**\/*Exception*.cs,**\/*Attribute*.cs\"
 & packages/dotnet-sonarscanner end /d:sonar.login="$env:SONARCLOUDTOKEN"
 & packages/csmacnz.Coveralls --opencover -i ./coverage.opencover.xml
--- coveralls.sh
+++ coveralls.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ -n "$COVERALLS_REPO_TOKEN" ]
-then
+#if [ -n "$COVERALLS_REPO_TOKEN" ]
+#then
   #packages/coveralls.net.0.7.0/tools/csmacnz.Coveralls.exe --opencover -i $(PWD)/Build.Tests/coverage.xml
-fi
+#fi
