443727d2656e7bbff1d24ffae2e3e558ca264e6f
hack2root <me@experimentalcommunity.org>
2018-07-05 18:48:36 +0300

    local coverage fix

--- coverage.cmd
+++ coverage.cmd
@@ -1,4 +1,15 @@
 @echo off
-powershell -file "./coverage.ps1"
+setlocal
+  
+  set /p DotNet_Version=<"%~dp0.config\DotNetCLIVersion.txt"
+  set DotNet_Path=%~dp0packages\dotnet\%DotNet_Version%
+  set NuGet_Path=%~dp0packages\nuget
+  set Tools_Path=%~dp0packages\tools
+
+  set "PATH=%DotNet_Path%;%NuGet_Path%;%Tools_Path%;%PATH%"
+  powershell -file "./coverage.ps1"
+
+endlocal
+
 pskill dotnet.exe
 powershell -command "rm -recurse -force .sonarqube"
