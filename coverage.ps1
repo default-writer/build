401a556310bd0b1897638d437cbb7e6d913fd780
hack2root <me@experimentalcommunity.org>
2019-03-03 23:37:28 +0300

    Update coverage.ps1

--- coverage.ps1
+++ coverage.ps1
@@ -28,7 +28,7 @@ if(![System.IO.File]::Exists($SonarScanner)) {
 & dotnet add Build.Tests package --package-directory packages/.packages coverlet.msbuild
 & dotnet restore
 
-& dotnet-sonarscanner begin /d:sonar.login="$env:SONARCLOUDTOKEN" /k:"build-core" /d:sonar.host.url="https://sonarcloud.io" /n:"build" /v:"1.0" /d:sonar.cs.opencover.reportsPaths="Build.Tests/coverage.opencover.xml" /d:sonar.coverage.exclusions="**/Interface*.cs,**/*Test*.cs,**/*Exception*.cs,**/*Attribute*.cs,**/Middleware/*.cs,/Pages/*.cs,**/Program.cs,**/Startup.cs,**/sample/*,**/aspnetcore/*,**/wwwroot/*,**/*.js,**/coverage.opencover.xml" /d:sonar.organization="hack2root-github" /d:sonar.sourceEncoding="UTF-8" /d:sonar.language=cs
+& dotnet-sonarscanner begin /o:"hack2root-github" /d:sonar.login="$env:SONARCLOUDTOKEN" /k:"build-core" /d:sonar.host.url="https://sonarcloud.io" /n:"build" /v:"1.0" /d:sonar.cs.opencover.reportsPaths="Build.Tests/coverage.opencover.xml" /d:sonar.coverage.exclusions="**/Interface*.cs,**/*Test*.cs,**/*Exception*.cs,**/*Attribute*.cs,**/Middleware/*.cs,/Pages/*.cs,**/Program.cs,**/Startup.cs,**/sample/*,**/aspnetcore/*,**/wwwroot/*,**/*.js,**/coverage.opencover.xml" /d:sonar.sourceEncoding="UTF-8" /d:sonar.language=cs
 & dotnet build --configuration Release
 & dotnet test --configuration Release --no-build Build.Tests /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 & dotnet-sonarscanner end /d:sonar.login="$env:SONARCLOUDTOKEN"
