e7a225ce4a4bb6ba20d9b41a453f926a31221dd3
hack2root <me@experimentalcommunity.org>
2019-03-03 20:24:45 +0300

    .appveyor/Dockerfile switched to 3.0.100-preview2

--- docker-init.ps1
+++ docker-init.ps1
@@ -1,5 +1,5 @@
-$container = "dot-net-sdk-3.0.100-preview2-nanoserver-1709"
-$image = "dot-net-sdk-3.0.100-preview2-nanoserver-1709:latest"
+$container = "dot-net-sdk-3.0.100-preview2"
+$image = "dot-net-sdk-3.0.100-preview2:latest"
 & docker --version
 & docker build -f .appveyour/Dockerfile -t $image . 
 & docker run --rm -it --name $container $image
