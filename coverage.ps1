b942bd2c8e838612c7c114630b9d8cb468c1ad90
hack2root <me@experimentalcommunity.org>
2019-03-03 22:53:04 +0300

    Update coverage.ps1
    Added custom dotnet cli version

--- coverage.ps1
+++ coverage.ps1
@@ -1,6 +1,14 @@
 & cd .nuget 
 & ./NuGet-dotnet-install
 & cd ..
+
+$DotNet_Version = [System.IO.File]::ReadAllText(".config/DotNetDotNetCliVersion.txt")
+$Tools_Path = Join-Path -Path $MyInvocation.MyCommand.Path -ChildPath "packages/tools"
+$DotNet_Path = Join-Path -Path $MyInvocation.MyCommand.Path -ChildPath "packages/dotnet/$DotNet_Version"
+$NuGet_Path = Join-Path -Path $MyInvocation.MyCommand.Path -ChildPath "packages/nuget"
+
+Set-Item -Path Env:PATH -Value ("$DotNet_Path;$NuGet_Path;$Tools_Path;" + $Env:PATH)
+
 if(![System.IO.File]::Exists("packages/tools/dotnet-sonarscanner.exe")) {
   # file with path $path doesn't exist
   dotnet tool install --tool-path packages/tools dotnet-sonarscanner
@@ -17,10 +25,10 @@ if(![System.IO.File]::Exists("packages/tools/csmacnz.Coveralls.exe")) {
 & dotnet add Build.Tests package --package-directory packages/.packages coverlet.msbuild
 & dotnet restore
 
-& ./packages/tools/dotnet-sonarscanner begin /d:sonar.login="$env:SONARCLOUDTOKEN" /k:"build-core" /d:sonar.host.url="https://sonarcloud.io" /n:"build" /v:"1.0" /d:sonar.cs.opencover.reportsPaths="Build.Tests/coverage.opencover.xml" /d:sonar.coverage.exclusions="**/Interface*.cs,**/*Test*.cs,**/*Exception*.cs,**/*Attribute*.cs,**/Middleware/*.cs,/Pages/*.cs,**/Program.cs,**/Startup.cs,**/sample/*,**/aspnetcore/*,**/wwwroot/*,**/*.js,**/coverage.opencover.xml" /d:sonar.organization="hack2root-github" /d:sonar.sourceEncoding="UTF-8" /d:sonar.language=cs
+& dotnet-sonarscanner begin /d:sonar.login="$env:SONARCLOUDTOKEN" /k:"build-core" /d:sonar.host.url="https://sonarcloud.io" /n:"build" /v:"1.0" /d:sonar.cs.opencover.reportsPaths="Build.Tests/coverage.opencover.xml" /d:sonar.coverage.exclusions="**/Interface*.cs,**/*Test*.cs,**/*Exception*.cs,**/*Attribute*.cs,**/Middleware/*.cs,/Pages/*.cs,**/Program.cs,**/Startup.cs,**/sample/*,**/aspnetcore/*,**/wwwroot/*,**/*.js,**/coverage.opencover.xml" /d:sonar.organization="hack2root-github" /d:sonar.sourceEncoding="UTF-8" /d:sonar.language=cs
 & dotnet build --configuration Release
 & dotnet test --configuration Release --no-build Build.Tests /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
-& ./packages/tools/dotnet-sonarscanner end /d:sonar.login="$env:SONARCLOUDTOKEN"
-& ./packages/tools/csmacnz.Coveralls --opencover -i Build.Tests/coverage.opencover.xml
+& dotnet-sonarscanner end /d:sonar.login="$env:SONARCLOUDTOKEN"
+& csmacnz.Coveralls --opencover -i Build.Tests/coverage.opencover.xml
 & dotnet remove Build.Tests package coverlet.msbuild
 & dotnet remove Build.Tests package OpenCover
