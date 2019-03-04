f0de5f7171448bd9674e49cd36b7c9b88085b94e
hack2root <me@experimentalcommunity.org>
2019-03-04 11:23:36 +0300

    Added OpenCover tests coverage

--- coverage.ps1
+++ coverage.ps1
@@ -3,42 +3,66 @@
 & cd ..
 
 $CurrentDir = Convert-Path .
-$DotNetCliVersionFile = Join-Path -Path $CurrentDir -ChildPath ".config/DotNetCliVersion.txt"
-$DotNet_Version = [System.IO.File]::ReadAllText($DotNetCliVersionFile)
+
+$DotNet_Version = [System.IO.File]::ReadAllText($(Join-Path -Path $CurrentDir -ChildPath ".config/DotNetCliVersion.txt"))
+
+$OpenCover_Version = "4.7.922"
+$XUnitRunnerConsole_Version = "2.4.1"
+$CoverletMsbuild_Version = "2.5.1"
+
 $Tools_Path = Join-Path -Path $CurrentDir -ChildPath  "packages/tools"
-$DotNet_Path = Join-Path -Path $CurrentDir -ChildPath "packages/dotnet/$DotNet_Version"
+$DotNet_Path = Join-Path -Path $CurrentDir -ChildPath "packages/dotnet/$($DotNet_Version)"
 $NuGet_Path = Join-Path -Path $CurrentDir -ChildPath  "packages/nuget"
-$SonarScanner = Join-Path -Path $CurrentDir -ChildPath "packages/tools/dotnet-sonarscanner.exe"
+$OpenCover_Path = Join-Path -Path $CurrentDir -ChildPath "packages/.packages/OpenCover/$($OpenCover_Version)/tools"
+$XUnitRunnerConsole_Path = Join-Path -Path $CurrentDir -ChildPath "packages/tools/xunit.runner.console.$($XUnitRunnerConsole_Version)/tools/netcoreapp2.0"
 
-Set-Item -Path Env:PATH -Value ("$DotNet_Path;$NuGet_Path;$Tools_Path;" + $Env:PATH)
+Set-Item -Path Env:PATH -Value ("$($DotNet_Path);$($NuGet_Path);$($Tools_Path);$($OpenCover_Path);" + $Env:PATH)
 
-if(![System.IO.File]::Exists($SonarScanner)) {
+Echo $Env:PATH
+
+if(![System.IO.File]::Exists($(Join-Path -Path $CurrentDir -ChildPath "packages/tools/dotnet-sonarscanner.exe"))) {
   # file with path $path doesn't exist
   dotnet tool install --tool-path packages/tools dotnet-sonarscanner
 } else {
   dotnet tool update --tool-path packages/tools dotnet-sonarscanner
 }
-if(![System.IO.File]::Exists($SonarScanner)) {
+if(![System.IO.File]::Exists($(Join-Path -Path $CurrentDir -ChildPath "packages/tools/csmacnz.Coveralls.exe"))) {
   # file with path $path doesn't exist
   dotnet tool install --tool-path packages/tools coveralls.net
 } else {
   dotnet tool update --tool-path packages/tools coveralls.net
 }
 
-#& dotnet add Build.Tests package --package-directory packages/.packages OpenCover
-#& dotnet add Build.Tests package --package-directory packages/.packages coverlet.msbuild
+#& dotnet add Build.Tests package OpenCover
+#& dotnet add Build.Tests package coverlet.msbuild
+#& dotnet add Build.Tests package xunit
+#& dotnet add Build.Tests package xunit.analyzers
+#& dotnet add Build.Tests package xunit.runner.console
+#& dotnet add Build.Tests package xunit.runner.visualstudio
+
 & dotnet restore
 
-& dotnet-sonarscanner begin /o:"hack2root-github" /d:sonar.login="$env:SONARCLOUDTOKEN" /k:"build-core" /d:sonar.host.url="https://sonarcloud.io" /n:"build" /v:"1.0" /d:sonar.cs.opencover.reportsPaths="Build.Tests/coverage.opencover.xml" /d:sonar.coverage.exclusions="**/Interface*.cs,**/*Test*.cs,**/*Exception*.cs,**/*Attribute*.cs,**/Middleware/*.cs,/Pages/*.cs,**/Program.cs,**/Startup.cs,**/sample/*,**/aspnetcore/*,**/wwwroot/*,**/*.js,**/coverage.opencover.xml" /d:sonar.sourceEncoding="UTF-8" /d:sonar.language=cs
+& dotnet-sonarscanner begin /o:"hack2root-github" /d:sonar.login="$env:SONARCLOUDTOKEN" /k:"build-core" /d:sonar.host.url="https://sonarcloud.io" /n:"build" /v:"1.0" /d:sonar.cs.opencover.reportsPaths="Build.Tests/coverage.opencover.xml" /d:sonar.coverage.inclusions="**/*.cs" /d:sonar.coverage.exclusions="**/Interface*.cs,**/*Test*.cs,**/*Exception*.cs,**/*Attribute*.cs,**/Middleware/*.cs,/Pages/*.cs,**/Program.cs,**/Startup.cs,**/sample/*,**/aspnetcore/*,**/wwwroot/*,**/xunit/*,**/*.js,**/coverage.opencover.xml" /d:sonar.sourceEncoding="UTF-8" /d:sonar.language=cs
 & dotnet build --configuration Release
 & dotnet test --configuration Release --no-build Build.Tests /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 & dotnet-sonarscanner end /d:sonar.login="$env:SONARCLOUDTOKEN"
 
-& nuget.exe install xunit.runner.console -OutputDirectory packages/tools -Version 2.3.1
-& nuget.exe install OpenCover -OutputDirectory packages/tools -Version 4.6.166
-& OpenCover.Console -register:user -target:dotnet.exe "-targetargs:""xunit.runner.console.2.3.1/tools/netcoreapp3.0/xunit.console.dll"" ""Build.Tests/bin/Release/netcoreapp3.0/Build.Tests.dll"" -noshadow -appveyor" -filter:"+[Build*]*" -oldStyle -output:opencoverCoverage.xml
-& csmacnz.Coveralls --opencover -i opencoverCoverage.xml --repoToken $env:COVERALLS_REPO_TOKEN --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID
 #& csmacnz.Coveralls --opencover -i Build.Tests/coverage.opencover.xml
 
-#& dotnet remove Build.Tests package coverlet.msbuild
-#& dotnet remove Build.Tests package OpenCover
+& dotnet add Build.Tests package --package-directory packages/.packages OpenCover --version $OpenCover_Version
+
+$DOTNET = Join-Path -Path $DotNet_Path -ChildPath "dotnet.exe"
+$OPENCOVER = Join-Path -Path $OpenCover_Path -ChildPath "OpenCover.Console.exe"
+
+& $OPENCOVER `
+  -target:"$($DOTNET)" `
+  -targetargs:"test -f netcoreapp3.0 -c Release Build.Tests/Build.Tests.csproj" `
+  -mergeoutput `
+  -hideskipped:File `
+  -output:"Build.Tests/opencover.xml" `
+  -oldStyle `
+  -filter:"+[Build*]* -[Build.Tests*]*" `
+  -searchdirs:"Build.Tests/bin/Release/netcoreapp3.0" `
+  -register:user
+
+& csmacnz.Coveralls --opencover -i "Build.Tests/opencover.xml"
