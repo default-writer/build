143582a08782fff7d9019c22545238ce35793867
hack2root <me@experimentalcommunity.org>
2019-03-06 01:24:11 +0300

    Update coverage.ps1

--- coverage.ps1
+++ coverage.ps1
@@ -40,7 +40,7 @@ if(![System.IO.File]::Exists($(Join-Path -Path $CurrentDir -ChildPath "packages/
 
 & dotnet-sonarscanner begin /o:"hack2root-github" /d:sonar.login="$env:SONARCLOUDTOKEN" /k:"build-core" /d:sonar.host.url="https://sonarcloud.io" /n:"build" /v:"1.0" /d:sonar.cs.opencover.reportsPaths="Build.Tests/coverage.opencover.xml" /d:sonar.coverage.inclusions="**/*.cs" /d:sonar.coverage.exclusions="**/Interface*.cs,**/*Test*.cs,**/*Exception*.cs,**/*Attribute*.cs,**/Middleware/*.cs,/Pages/*.cs,**/Program.cs,**/Startup.cs,**/sample/*,**/aspnetcore/*,**/wwwroot/*,**/xunit/*,**/*.js,**/coverage.opencover.xml" /d:sonar.sourceEncoding="UTF-8" /d:sonar.language=cs
 & dotnet build --configuration Release
-& dotnet test --configuration Release --no-build Build.Tests /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:Include="[build.*]*" /p:Exclude="[xunit*]*,[build.abstractions.*]*" -v:n
+& dotnet test --configuration Release --no-build Build.Tests /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:Include="[build.*]*" /p:Exclude="[xunit*]*" -v:n
 & dotnet-sonarscanner end /d:sonar.login="$env:SONARCLOUDTOKEN"
 
 # Try to upload to Coveralls
