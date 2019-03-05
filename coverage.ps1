240475aa2e66f00860966b1ef1153eefa7c69a8c
hack2root <me@experimentalcommunity.org>
2019-03-06 01:17:23 +0300

    Update coverage.ps1

--- coverage.ps1
+++ coverage.ps1
@@ -8,7 +8,8 @@ $DotNet_Version = [System.IO.File]::ReadAllText($(Join-Path -Path $CurrentDir -C
 
 $OpenCover_Version = "4.7.922"
 $XUnitRunnerConsole_Version = "2.4.1"
-$CoverletMsbuild_Version = "2.5.1"
+# Moving from version 2.5.1 to 2.6.0 requires /p:Include=[build.*]* /p:Exclude="[xunit*]*,[build.abstractions.*]*"
+$CoverletMsbuild_Version = "2.6.0"
 
 $Tools_Path = Join-Path -Path $CurrentDir -ChildPath  "packages/tools"
 $DotNet_Path = Join-Path -Path $CurrentDir -ChildPath "packages/dotnet/$($DotNet_Version)"
@@ -39,27 +40,27 @@ if(![System.IO.File]::Exists($(Join-Path -Path $CurrentDir -ChildPath "packages/
 
 & dotnet-sonarscanner begin /o:"hack2root-github" /d:sonar.login="$env:SONARCLOUDTOKEN" /k:"build-core" /d:sonar.host.url="https://sonarcloud.io" /n:"build" /v:"1.0" /d:sonar.cs.opencover.reportsPaths="Build.Tests/coverage.opencover.xml" /d:sonar.coverage.inclusions="**/*.cs" /d:sonar.coverage.exclusions="**/Interface*.cs,**/*Test*.cs,**/*Exception*.cs,**/*Attribute*.cs,**/Middleware/*.cs,/Pages/*.cs,**/Program.cs,**/Startup.cs,**/sample/*,**/aspnetcore/*,**/wwwroot/*,**/xunit/*,**/*.js,**/coverage.opencover.xml" /d:sonar.sourceEncoding="UTF-8" /d:sonar.language=cs
 & dotnet build --configuration Release
-& dotnet test --configuration Release --no-build Build.Tests /p:CollectCoverage=true /p:CoverletOutputFormat=opencover -v:n
+& dotnet test --configuration Release --no-build Build.Tests /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:Include="[build.*]*" /p:Exclude="[xunit*]*,[build.abstractions.*]*" -v:n
 & dotnet-sonarscanner end /d:sonar.login="$env:SONARCLOUDTOKEN"
 
-# Do NOT upload to Coveralls
-#& csmacnz.Coveralls --opencover -i Build.Tests/coverage.opencover.xml
+# Try to upload to Coveralls
+& csmacnz.Coveralls --opencover -i Build.Tests/coverage.opencover.xml
 
-& dotnet add Build.Tests package --package-directory packages/.packages OpenCover --version $OpenCover_Version
-
-$DOTNET = Join-Path -Path $DotNet_Path -ChildPath "dotnet.exe"
-$OPENCOVER = Join-Path -Path $OpenCover_Path -ChildPath "OpenCover.Console.exe"
-
-& $OPENCOVER `
-  -target:"$($DOTNET)" `
-  -targetargs:"test -f netcoreapp3.0 -c Release Build.Tests/Build.Tests.csproj" `
-  -mergeoutput `
-  -hideskipped:File `
-  -output:"Build.Tests/opencover.xml" `
-  -oldStyle `
-  -filter:"+[Build*]* -[Build.Tests*]* -[Build*]*AttributeProvider*" `
-  -searchdirs:"Build.Tests/bin/Release/netcoreapp3.0" `
-  -register:user
-
+#& dotnet add Build.Tests package --package-directory packages/.packages OpenCover --version $OpenCover_Version
+#
+#$DOTNET = Join-Path -Path $DotNet_Path -ChildPath "dotnet.exe"
+#$OPENCOVER = Join-Path -Path $OpenCover_Path -ChildPath "OpenCover.Console.exe"
+#
+#& $OPENCOVER `
+#  -target:"$($DOTNET)" `
+#  -targetargs:"test -f netcoreapp3.0 -c Release Build.Tests/Build.Tests.csproj" `
+#  -mergeoutput `
+#  -hideskipped:File `
+#  -output:"Build.Tests/opencover.xml" `
+#  -oldStyle `
+#  -filter:"+[Build*]* -[Build.Tests*]* -[Build*]*AttributeProvider*" `
+#  -searchdirs:"Build.Tests/bin/Release/netcoreapp3.0" `
+#  -register:user
+#
 # Upload to coveralls FILERED sources
-& csmacnz.Coveralls --opencover -i "Build.Tests/opencover.xml"  --useRelativePaths
+#& csmacnz.Coveralls --opencover -i "Build.Tests/opencover.xml"  --useRelativePaths
