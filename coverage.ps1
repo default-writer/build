7c2c2d818b4ccd54813d2f9b1ab932701b91b54c
hack2root <me@experimentalcommunity.org>
2019-03-05 23:45:10 +0300

    update pipeline

--- coverage.ps1
+++ coverage.ps1
@@ -39,7 +39,7 @@ if(![System.IO.File]::Exists($(Join-Path -Path $CurrentDir -ChildPath "packages/
 
 & dotnet-sonarscanner begin /o:"hack2root-github" /d:sonar.login="$env:SONARCLOUDTOKEN" /k:"build-core" /d:sonar.host.url="https://sonarcloud.io" /n:"build" /v:"1.0" /d:sonar.cs.opencover.reportsPaths="Build.Tests/coverage.opencover.xml" /d:sonar.coverage.inclusions="**/*.cs" /d:sonar.coverage.exclusions="**/Interface*.cs,**/*Test*.cs,**/*Exception*.cs,**/*Attribute*.cs,**/Middleware/*.cs,/Pages/*.cs,**/Program.cs,**/Startup.cs,**/sample/*,**/aspnetcore/*,**/wwwroot/*,**/xunit/*,**/*.js,**/coverage.opencover.xml" /d:sonar.sourceEncoding="UTF-8" /d:sonar.language=cs
 & dotnet build --configuration Release
-& dotnet test --configuration Release --no-build Build.Tests /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+& dotnet test --configuration Release /p:CollectCoverage=true /p:CoverletOutputFormat=opencover -v:n
 & dotnet-sonarscanner end /d:sonar.login="$env:SONARCLOUDTOKEN"
 
 # Do NOT upload to Coveralls
