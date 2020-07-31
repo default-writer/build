c18217dc913b7a4fa50cfb68e84c4428d2231d4e
Artur Mustafin <artur.mustafin@gmail.com>
2020-07-31 18:10:13 +0500

    readme fix

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 ## Step 1: Authenticate (if this is the first time)
-#dotnet nuget add source https://nuget.pkg.github.com/hack2root/index.json -n github -u hack2root -p GH_TOKEN
+#dotnet nuget add source https://nuget.pkg.github.com/hack2root/index.json -n github -u hack2root -p $GH_TOKEN
 ## Step 2: Pack
 #dotnet pack --configuration Release GitHub.Build.sln
 ## Step 3: Publish
