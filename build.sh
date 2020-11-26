0a788e301d28c7e0cd1057c5aea45364abc9b53c
funcelot <artur.mustafin@gmail.com>
2020-11-26 13:46:44 +0000

    updated to .net 5

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update; \
   sudo apt-get install -y apt-transport-https && \
   sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-3.1
-sudo apt-get install -y dotnet-runtime-3.1
+  sudo apt-get install -y dotnet-sdk-5.0
+sudo apt-get install -y dotnet-runtime-5.0
