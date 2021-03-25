d67950bb12ac7242fb090dd66f54a6b7f8406a2c
Artur Mustafin <funcelot@outlook.com>
2021-03-26 02:45:57 +0300

    Update docker-init.ps1

--- docker-init.ps1
+++ docker-init.ps1
@@ -1,7 +1,6 @@
 Get-Host | Select-Object Version
 $container = "dot-net-sdk-5.0.100"
 $image = "dot-net-sdk-5.0.100:latest"
-& dotnet --list-sdks
 & docker --version
 & docker build -f .appveyour/Dockerfile -t $image . 
 & docker run --rm -it --name $container $image
