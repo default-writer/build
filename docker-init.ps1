18cf48559f372579898c2a9975a68cefd7c8f936
funcelot <artur.mustafin@gmail.com>
2021-03-26 07:47:37 +0300

    updated SDK to version 5.0.201

--- docker-init.ps1
+++ docker-init.ps1
@@ -1,6 +1,6 @@
 Get-Host | Select-Object Version
-$container = "dot-net-sdk-5.0.100"
-$image = "dot-net-sdk-5.0.100:latest"
+$container = "dot-net-sdk-5.0.201"
+$image = "dot-net-sdk-5.0.201:latest"
 & docker --version
 & docker build -f .appveyour/Dockerfile -t $image . 
 & docker run --rm -it --name $container $image
