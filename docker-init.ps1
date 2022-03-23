702199be322c621eb74a8a36d4245b83acffd6c1
Artur Mustafin <artur.mustafin@wickes.co.uk>
2022-03-23 09:44:00 +0300

    switched to 6.0.201

--- docker-init.ps1
+++ docker-init.ps1
@@ -1,6 +1,6 @@
 Get-Host | Select-Object Version
-$container = "dot-net-sdk-5.0.201"
-$image = "dot-net-sdk-5.0.201:latest"
+$container = "dot-net-sdk-6.0.201"
+$image = "dot-net-sdk-6.0.201:latest"
 & docker --version
-& docker build -f .appveyour/Dockerfile -t $image . 
+& docker build -f .appveyour/Dockerfile -t $image .
 & docker run --rm --name $container $image
