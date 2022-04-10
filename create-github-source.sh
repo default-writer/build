44baa5168f44b1754041866654aebd6537ad6874
Artur Mustafin <artur.mustafin@wickes.co.uk>
2022-04-10 14:57:30 +0000

    added hotcompilable project settings

--- create-github-source.sh
+++ create-github-source.sh
@@ -1,2 +1,3 @@
-#!/bin/env/bash -e
-dotnet nuget add source --username ${GITHUB_USERNAME} --password ${GITHUB_TOKEN} --store-password-in-clear-text --name github "https://nuget.pkg.github.com/${GINTHUB_USERNAME}/index.json"
+#!/usr/bin/env bash
+set -e
+dotnet nuget add source --username ${GITHUB_USERNAME} --password ${GITHUB_TOKEN} --store-password-in-clear-text --name github "https://nuget.pkg.github.com/${GITHUB_USERNAME}/index.json"
