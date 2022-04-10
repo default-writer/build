5d604e1c7d31ab4c3081ffd0dd883b7a37dbf5b5
Artur Mustafin <funcelot@outlook.com>
2022-04-10 18:10:42 +0000

    added additiona commands to bootstrap containers

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 set -e
-wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get update -y; \
-  sudo apt-get install -y apt-transport-https && \
-  sudo apt-get update && \
-  sudo apt-get install -y dotnet-sdk-6.0
-sudo apt-get install -y dotnet-runtime-6.0
-sudo dotnet build
+dotnet restore
+dotnet test
+dotnet build
